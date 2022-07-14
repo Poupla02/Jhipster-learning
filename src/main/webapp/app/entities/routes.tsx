b036ff92e6163986f1277e63912a6c6f53f3c312
annicksome26 <annicksome26@gmail.com>
2022-07-14 11:16:16 +0000

    Add a good file jdl in project and edit getAllBlogs: get blogs of current user

--- src/main/webapp/app/entities/routes.tsx
+++ src/main/webapp/app/entities/routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
+import Blog from './blog';
+import Post from './post';
+import Tag from './tag';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 export default ({ match }) => {
@@ -9,6 +12,9 @@ export default ({ match }) => {
     <div>
       <Switch>
         {/* prettier-ignore */}
+        <ErrorBoundaryRoute path={`${match.url}blog`} component={Blog} />
+        <ErrorBoundaryRoute path={`${match.url}post`} component={Post} />
+        <ErrorBoundaryRoute path={`${match.url}tag`} component={Tag} />
         {/* jhipster-needle-add-route-path - JHipster will add routes here */}
       </Switch>
     </div>
